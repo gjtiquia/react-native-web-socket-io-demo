7aaa811952828f02a9901bd4ff7d5e6325e5584e
GJ Tiquia <gershom.tiquia@gmail.com>
2023-06-03 12:48:53 +0800

    feat: able to fetch random data from random api in useEffect

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,7 @@ import os from "os";
 const app = express();
 const HOST_IP = "0.0.0.0"; // Setting to 0.0.0.0 allows to listen to  computer ip or all network interfaces of the computer. Ref: https://stackoverflow.com/questions/21263021/connect-to-another-computer-using-ip-address?rq=4
 const HOSTNAME = os.hostname();
-const PORT = 8080;
+const PORT = 8080; // Arbitrary port
 
 console.log(`Hostname: ${HOSTNAME}`);
 
--- src/Main.tsx
+++ src/Main.tsx
@@ -5,16 +5,31 @@ import { HelloWorld } from "src/components"
 
 const Main = () => {
     const [isMounted, setIsMounted] = useState(false);
+    const [apiData, setApiData] = useState<any>({ "state": "loading" });
 
-    useEffect(() => setIsMounted(true), []);
+    useEffect(() => {
+        console.log("Fetching...");
+
+        // fetch("http://localhost:8080") // Not working, maybe need to set up cors?
+        fetch("https://random-data-api.com/api/v2/users?size=2")
+            .then(response => response.json())
+            .then(data => {
+                setApiData(data);
+                setIsMounted(true)
+            })
+            .catch(response => console.error(response));
+    }, []);
 
     return (
         <SafeAreaProvider>
-            <View className="flex-1 flex-row justify-center items-center">
-                <Text>{"Is Mounted: "}</Text>
-                <Text>{isMounted ? "True" : "False"}</Text>
+            <View className="flex-1 justify-center items-center">
+                <View className="flex flex-row">
+                    <Text>{"Is Mounted: "}</Text>
+                    <Text>{isMounted ? "True" : "False"}</Text>
+                </View>
+                <Text>{JSON.stringify(apiData)}</Text>
             </View>
-        </SafeAreaProvider>
+        </SafeAreaProvider >
     );
 }
 
